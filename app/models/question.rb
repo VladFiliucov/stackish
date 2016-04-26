c2c5302b3769ca18b212b8a4ab3672cbf8f1fc1d
VladFiliucov <filiucov@mail.ru>
2016-04-26 17:37:46 +0100

    Validate presence and length of Question.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   validates :title, presence: true
+  validates :body, presence: true
+  validates_length_of :title, minimum: 7
+  validates_length_of :body, minimum: 10
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+
+  it do
+    should validate_length_of(:title).
+      is_at_least(7).
+      on(:create)
+  end
+
+  it do
+    should validate_length_of(:body).
+      is_at_least(10).
+      on(:create)
+  end
 end
