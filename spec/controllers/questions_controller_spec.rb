9087adfb46ba4cd5da750fead1915ca8436cbc78
VladFiliucov <filiucov@mail.ru>
2016-04-27 19:02:53 +0100

    Add show action.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
 
     it 'populates array of questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -14,6 +12,20 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders index template' do
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question)}
+
+    before {get :show, id: question}
+
+    it 'assigns requested question to @question' do
+      expect(assigns(:question)).to eq(question)
+    end
+
+    it 'render show template' do
+      expect(response).to render_template(:show)
     end
   end
 end
