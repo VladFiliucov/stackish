102af16c1b5dbc564921aa20a0f59f2e0ee9c90a
VladFiliucov <filiucov@mail.ru>
2016-04-30 15:40:08 +0100

    Add user registration happy path.

---
+++ spec/feature/user/register_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "Registration" do
+  it "allows a user to signup." do
+    expect(User.count).to eq(0)
+
+    visit root_path
+    expect(page).to have_content("Sign Up")
+    click_link("Sign Up")
+
+    fill_in "Email", with: "uniqemail@example.com"
+    fill_in "Password", with: "SuperPassword123"
+    fill_in "Password confirmation", with: "SuperPassword123"
+    click_button "Sign up"
+
+    expect(User.count).to eq(1)
+  end
+end
