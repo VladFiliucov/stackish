726d2577e604aa064f5c7ea6c903797f903ba97c
VladFiliucov <filiucov@mail.ru>
2016-05-30 16:42:31 +0100

    Add posibility to add and delete attachments to already created answers.

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :attachment do
-    file "MyString"
+    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'features', 'features_helper.rb')) }
+    factory :attachment_for_answer do
+      association :attachable, factory: :answer
+    end
   end
 end
--- spec/features/answer/attach_files_to_answer_spec.rb
+++ spec/features/answer/attach_files_to_answer_spec.rb
@@ -9,35 +9,69 @@ feature 'Attach file to answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  background do
-    sign_in(user)
-    visit question_path(question)
-    fill_in 'My Answer', with: "This is a answer for the question"
-    attach_file "File", "#{Rails.root}/spec/rails_helper.rb"
-  end
+  context 'Owner' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+      fill_in 'My Answer', with: "This is a answer for the question"
+      attach_file "File", "#{Rails.root}/spec/rails_helper.rb"
+    end
 
-  scenario 'User attaches file on question creation', js: true do
-    click_on 'Answer'
+    scenario 'User attaches file on question creation', js: true do
+      click_on 'Answer'
 
-    within ".answers" do
+      within ".answers" do
+        expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      end
+    end
+
+    scenario 'User can attach multiple files to his answer', js: true do
+      click_on 'Attach File'
+      within all(:css, ".nested-fields").last do
+        attach_file "File", "#{Rails.root}/app/models/question.rb"
+      end
+      click_on 'Answer'
+
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      expect(page).to have_link 'question.rb', href: '/uploads/attachment/file/2/question.rb'
     end
+
+    scenario 'User can delete attachments', js: true do
+      click_on 'Delete Attachment'
+
+      expect(page).to_not have_content("rails_helper.rb")
+    end
+
   end
 
-  scenario 'User can attach multiple files to his answer', js: true do
-    click_on 'Attach File'
-    within all(:css, ".nested-fields").last do
-      attach_file "File", "#{Rails.root}/app/models/question.rb"
+  context 'Already existing Answer' do
+    let!(:another_question) { create(:question) }
+    let!(:answers_author) { create(:user) }
+    let!(:answer) { create(:answer, question: another_question, user: answers_author) }
+
+    before do
+      sign_in(answers_author)
+      visit question_path(another_question)
+      click_on 'Edit Answer'
+      within "#edit-answer-#{answer.id}" do
+        click_on 'Attach File'
+        attach_file "File", "#{Rails.root}/app/models/answer.rb"
+      end
+      click_on 'Edit'
     end
-    click_on 'Answer'
 
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
-    expect(page).to have_link 'question.rb', href: '/uploads/attachment/file/2/question.rb'
-  end
+    scenario 'Can attach files after answer is created', js: true do
+      expect(page).to have_content("answer.rb")
+    end
 
-  scenario 'User can delete attachments', js: true do
-    click_on 'Delete Attachment'
+    scenario 'Can delete attachment of existing answer', js: true do
+      click_on 'Edit Answer'
+      within "#edit-answer-#{answer.id}" do
+        click_on('Delete Attachment')
+      end
+      click_on 'Edit'
 
-    expect(page).to_not have_content("rails_helper.rb")
+      expect(page).to_not have_content("answer.rb")
+    end
   end
 end
