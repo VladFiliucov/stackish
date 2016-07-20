4bde40ab35f4f94b1e276622be74fc6c5ba105a9
VladFiliucov <filiucov@mail.ru>
2016-07-20 20:46:49 +0300

    Add user - vote reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   def author?(object)
     self.id == object.user_id
