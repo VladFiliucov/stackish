01b40b8d48ff876106b43b814536e8d3c22e3e94
VladFiliucov <filiucov@mail.ru>
2016-07-23 19:47:24 +0300

    Write specs and regroup them slightly

--- spec/models/concerns/votable_spec.rb
+++ spec/models/concerns/votable_spec.rb
@@ -14,16 +14,41 @@ RSpec.describe Votable do
 
   let(:user) { create(:user) }
   let(:another_user) { create(:user) }
+  let(:and_another_user) { create(:user) }
+  let(:yet_another_user) { create(:user) }
   let(:entry) { WithVotable.create }
 
-  describe 'increase rating' do
+  describe '#rate' do
+    it 'decreases objects rate' do
+      expect {
+        entry.rate(user, -1)
+      }.to change(entry.votes, :count).by(1)
+      expect(entry.current_rating).to eq(-1)
+    end
+
     it 'increases objects rate' do
       expect { entry.rate(user, 1)
       }.to change(entry.votes, :count).by(1)
-      expect(entry.current_rating).to eq 1
+      expect(entry.current_rating).to eq(1)
+    end
+
+    it 'updates rating if user exists' do
+      entry.rate(user, 1)
+      expect {
+        entry.rate(user, -1)
+      }.to_not change(entry.votes, :count)
+      expect(entry.current_rating).to eq(-1)
     end
   end
 
-  describe 'decrease rating' do
+  describe '#current_rating' do
+    it 'calculates current rating properly' do
+      entry.rate(user, 1)
+      entry.rate(another_user, 1)
+      entry.rate(yet_another_user, 1)
+      entry.rate(and_another_user, -1)
+      expect(entry.votes.count).to eq(4)
+      expect(entry.current_rating).to eq(2)
+    end
   end
 end
