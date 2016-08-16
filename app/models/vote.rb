fa0cfbc28c0532a1c1773d9dd278f5f79bcc2b19
VladFiliucov <filiucov@mail.ru>
2016-08-16 20:50:17 +0300

    Remove redundant validation.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates :user_id, :votable_id, :votable_type, presence: true
+  validates :user_id,  presence: true
   validates :rate_point, inclusion: { in: -1..1 }
 end
