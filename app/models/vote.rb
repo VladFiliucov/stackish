596f6e090f6a96fe4207f882e344205875385475
VladFiliucov <filiucov@mail.ru>
2016-08-16 20:44:25 +0300

    Use new validation.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
 
   validates :user_id, :votable_id, :votable_type, presence: true
-  validates_inclusion_of :rate_point, in: -1..1
+  validates :rate_point, inclusion: { in: -1..1 }
 end
