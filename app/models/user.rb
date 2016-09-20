06527f5dbba8db0c5df8f3822fb8a1ed7a9ea093
VladFiliucov <filiucov@mail.ru>
2016-09-20 20:52:35 +0100

    implement facebook sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,17 @@ class User < ActiveRecord::Base
 
     email = auth.info[:email]
     user = User.where(email: email).first
-    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
