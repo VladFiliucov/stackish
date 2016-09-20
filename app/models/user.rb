2f91e9f0ac065d1c114f7911254c12c722e6d862
VladFiliucov <filiucov@mail.ru>
2016-09-20 20:03:20 +0100

    Add authorization model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :votes, as: :votable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def author?(object)
     self.id == object.user_id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User do
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:votes).dependent(:destroy) }
   it { should have_many(:comments).dependent(:destroy)}
+  it { should have_many(:authorizations).dependent(:destroy)}
 
   describe "Verify ownership with #author?" do
     let(:user) { create(:user)}
@@ -26,12 +27,26 @@ RSpec.describe User do
 
   describe ".find_for_oauth" do
     let!(:user) { create(:user) }
-    let(:auth) { :Omniauth::AuthHash.new(provider: 'facebook', uid: '12345') }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '12345') }
 
     context 'user already has authorization' do
       it 'it returns the user' do
         user.authorizations.create(provider: 'facebook', uid: '12345')
         expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has no authorization' do
+      context 'existing user' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '12345', info: { email: user.email }) }
+
+        it 'does not create new user' do
+          expect {User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'creates authorization for user' do
+          expect {User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+        end
       end
     end
   end
