3bbdc19c190774c406197f06b9e4de5f668e7ad1
VladFiliucov <filiucov@mail.ru>
2016-09-23 20:02:15 +0100

    remove duplicated code in oauth controller to a private method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
+    generic_auth('facebook')
   end
 
   def twitter
+    generic_auth('twitter')
+  end
+
+  private
+
+  def generic_auth(kind)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: kind.capitalize) if is_navigational_format?
     end
   end
 end
