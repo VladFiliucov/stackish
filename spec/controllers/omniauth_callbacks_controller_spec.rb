ebd8612eae547bd8e9d9794aed8399c800273ca2
VladFiliucov <filiucov@mail.ru>
2016-09-27 10:27:40 +0100

    omniauth callbacks controller specs

---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe OmniauthCallbacksController, type: :controller do
+  let(:user) { create(:user) }
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe 'GET #facebook' do
+    before do
+      request.env["omniauth.auth"] = OmniAuth::AuthHash.new(
+        {
+          provider: 'facebook',
+          uid: '555555',
+          info: {
+            email: 'email@gmail.com'
+          }
+        }
+      )
+      get :facebook
+    end
+
+    it 'assigns @user' do
+      expect(assigns(:user)).to be_an_instance_of(User)
+    end
+
+    it { should respond_with(302) }
+    it { should redirect_to root_path }
+  end
+
+  describe 'GET #twitter' do
+    before do
+      request.env["omniauth.auth"] = OmniAuth::AuthHash.new(
+        {
+          provider: 'twitter',
+          uid: '555555',
+          info: {
+            email: 'email@gmail.com'
+          }
+        }
+      )
+      get :twitter
+    end
+
+    it 'assigns @user' do
+      expect(assigns(:user)).to be_an_instance_of(User)
+    end
+
+    it { should respond_with(302) }
+    it { should redirect_to root_path }
+  end
+end
