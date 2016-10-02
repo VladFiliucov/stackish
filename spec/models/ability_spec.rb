7c5e3e3275070fcd9c5609bb164da9bfcd01b596
VladFiliucov <filiucov@mail.ru>
2016-10-02 23:11:55 +0100

    Add abilities for ordinary user.

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -18,4 +18,25 @@ describe Ability do
 
     it { should be_able_to :manage, :all }
   end
+
+  describe 'user ability' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    it { should_not be_able_to :manage, :all}
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+
+    it { should be_able_to :update, create(:question, user: user), user: user }
+    it { should_not be_able_to :update, create(:question, user: other_user), user: user }
+
+    it { should be_able_to :update, create(:answer, user: user), user: user }
+    it { should_not be_able_to :update, create(:answer, user: other_user), user: user }
+
+    it { should be_able_to :update, create(:comment, user: user), user: user }
+    it { should_not be_able_to :update, create(:comment, user: other_user), user: user }
+  end
 end
