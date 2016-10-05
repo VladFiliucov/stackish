c0a5ba0eaf5e27377cfc282c1bc68eafddc618b0
VladFiliucov <filiucov@mail.ru>
2016-10-05 22:37:00 +0100

    Authentication /me action in api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
   use_doorkeeper
+
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
+  end
+
   get 'comments/create'
 
   resources :attachments, only: [:destroy]
