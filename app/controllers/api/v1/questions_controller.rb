69c6c6ce884cea3d606087705385e2d7d035a1fc
VladFiliucov <filiucov@mail.ru>
2016-10-08 20:58:20 +0100

    Add authorize resource for questions api controller.

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  authorize_resource class: User
+
   def index
     render nothing: true
   end
--- spec/api/v1/profiles_controller_spec.rb
+++ spec/api/v1/profiles_controller_spec.rb
@@ -15,28 +15,26 @@ describe 'Profile API' do
     end
 
     context 'authenticated' do
-      context 'admin' do
-        let!(:me) { create(:user, admin: true) }
-        let!(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      let!(:me) { create(:user, admin: true) }
+      let!(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-        before do
-          get '/api/v1/profiles/me', format: :json, access_token: access_token.token
+      before do
+        get '/api/v1/profiles/me', format: :json, access_token: access_token.token
+      end
+
+      it 'returns success status' do
+        expect(response).to be_success
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains {attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
         end
+      end
 
-        it 'returns success status' do
-          expect(response).to be_success
-        end
-
-        %w(id email created_at updated_at admin).each do |attr|
-          it "contains {attr}" do
-            expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
-          end
-        end
-
-        %w(password encrypted_password).each do |attr|
-          it "does not contain #{attr}" do
-            expect(response.body).to_not have_json_path(attr)
-          end
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
         end
       end
     end
