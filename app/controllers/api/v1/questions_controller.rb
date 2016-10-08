94d7bd787c937eb0edd51c76c140c8ab74ab090b
VladFiliucov <filiucov@mail.ru>
2016-10-08 21:38:10 +0100

    Include answers in json response for question.

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   authorize_resource class: User
 
   def index
-    render nothing: true
+    @qestions = Question.all
+    respond_with @qestions.to_json(include: :answers)
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -16,10 +16,38 @@ describe 'Questions API' do
 
     context 'authorized' do
       let!(:access_token) { create(:access_token) }
+      let!(:questions) { create_list(:question, 2) }
+      let(:question) { questions.first }
+      let!(:answer) { create(:answer, question: question) }
+
+      before do
+        get '/api/v1/questions', format: :json, access_token: access_token.token
+      end
 
       it 'returns status code 200' do
-        get '/api/v1/questions', format: :json, access_token: access_token.token
         expect(response).to be_success
+      end
+
+      it 'returns list of questions' do
+        expect(response.body).to have_json_size(2)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
+      end
+
+      context 'with answers' do
+        it 'included' do
+          expect(response.body).to have_json_size(1).at_path("0/answers")
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "questions answer object contains #{attr}" do
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+          end
+        end
       end
     end
   end
