fd9687188b0494a885478a717d9bc4b3f319a290
VladFiliucov <filiucov@mail.ru>
2016-11-03 18:08:59 +0000

    Fix subscription controller specs.

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SubscriptionsController, type: :controller do
   describe "GET #create" do
     context 'guest' do
       it "returns unauthorized" do
-        post :create, id: question.id, format: :json
+        post :create, question_id: question.id, format: :js
         expect(response).to have_http_status :unauthorized
       end
     end
@@ -16,12 +16,12 @@ RSpec.describe SubscriptionsController, type: :controller do
       login_user
 
       it "returns http success" do
-        post :create, id: question.id, format: :json
+        post :create, question_id: question.id, format: :json
         expect(response.status).to eq(201)
       end
 
       it "creates subscription" do
-        expect { post :create, id: question.id, format: :json }.to change(question.subscriptions, :count).by 1
+        expect { post :create, question_id: question.id, format: :js }.to change(question.subscriptions, :count).by 1
       end
     end
   end
@@ -31,7 +31,7 @@ RSpec.describe SubscriptionsController, type: :controller do
       let(:subscription) { create(:subscription, user: user, question: question) }
 
       it "returns unauthorized" do
-        delete :destroy, id: question, format: :json
+        delete :destroy, question_id: question.id, id: subscription.id, format: :js
         expect(response).to have_http_status :unauthorized
       end
     end
@@ -41,12 +41,12 @@ RSpec.describe SubscriptionsController, type: :controller do
       let!(:subscription) { create(:subscription, user: @user, question: question) }
 
       it "returns http success" do
-        delete :destroy, id: question, format: :json
+        delete :destroy, question_id: question.id, id: subscription.id, format: :js
         expect(response).to have_http_status(:success)
       end
 
       it "deletes subscription" do
-        expect { delete :destroy, id: question, format: :json }.to change(question.subscriptions, :count).by(-1)
+        expect { delete :destroy, question_id: question.id, id: subscription.id, format: :js }.to change(question.subscriptions, :count).by(-1)
       end
     end
   end
