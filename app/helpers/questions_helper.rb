35ec570a6c47bb334c1ef5308e782f07946b02b5
VladFiliucov <filiucov@mail.ru>
2016-11-04 12:13:08 +0000

    Add memoization

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,10 @@
 module QuestionsHelper
   def trigger_subscription(user, question)
-    subscription = user.subscriptions.find_by(question_id: question.id)
+    subscription ||= user.subscriptions.find_by(question_id: @question.id)
     if subscription
       link_to 'Unsubscribe', question_subscription_path(@question, subscription), class: 'btn btn-primary', remote: true, method: :delete
     else
-      link_to 'Subscribe', question_subscriptions_path(question), class: 'btn btn-primary', remote: true, method: :post
+      link_to 'Subscribe', question_subscriptions_path(@question), class: 'btn btn-primary', remote: true, method: :post
     end
   end
 end
