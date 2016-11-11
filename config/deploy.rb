001f10b1dca3bc36e1876b7ccec10a228fafbaef
VladFiliucov <filiucov@mail.ru>
2016-11-11 08:24:18 +0000

    prepare deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,32 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :application, 'stackish'
+set :repo_url, 'git@github.com:VladFiliucov/stackish.git'
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
+set :deploy_to, '/home/deploy/stackish'
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/private_pub.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+namespace :deploy do
+  desc 'Restarting Sidekiq'
+  task :restart_sidekiq do
+    on roles(:worker) do
+      execute :service, "sidekiq restart"
+    end
+  end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  after "deploy:published", "restart_sidekiq"
+
+  task :restart do
+    on roles(:app) do
+      execute :touch, release_path.join("tmp/restart.txt")
+    end
+  end
+  after "deploy:published", "restart"
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,10 +17,11 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{deploy@138.68.130.204}
+role :web, %w{deploy@138.68.130.204}
+role :db,  %w{deploy@138.68.130.240}
 
+set :rails_env, :production
 
 
 # Configuration
@@ -49,13 +50,12 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server '138.68.130.240',
+  user: 'deploy',
+  roles: %w{web app db},
+  primary: true
+  ssh_options: {
+    keys: %w(/home/vladislavfiliucov/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(publickey password)
+  }
