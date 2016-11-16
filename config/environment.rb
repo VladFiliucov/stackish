eea180782aff0f799390df24d555786470698c68
VladFiliucov <filiucov@mail.ru>
2016-11-16 20:45:10 +0000

    Configure SMTP with Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'vladfiliucov',
+  :password => Rails.application.secrets.sendgrid_api_key,
+  :domain => '138.68.130.204',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
